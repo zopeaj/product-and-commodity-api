217ab29d98e8ddd8651c8610f9477667ae2e29a5
S. Ezekiel Korede <59841476+zopeaj@users.noreply.github.com>
2023-03-27 10:34:28 +0100

    update DataSourceConfiguration.java

--- src/main/java/com/zopeaj/commodityapi/config/DataSourceConfiguration.java
+++ src/main/java/com/zopeaj/commodityapi/config/DataSourceConfiguration.java
@@ -1,2 +1,6 @@
 package com.zopeaj.commodityapi.config;
 
+import java.sql.DataSource;
+
+import org.springframework.context.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
